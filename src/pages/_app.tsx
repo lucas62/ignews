906b853854b28b59c28734574eca2051a950bf25
Lucas Anselmo <30813248+lucas62@users.noreply.github.com>
2022-07-26 18:18:31 -0300

    Configure global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "../../node_modules/next/app"
 
+import '../styles/global.scss'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
