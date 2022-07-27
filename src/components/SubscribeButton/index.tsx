4129005e32fa3497299d0c123dc0fa6d08cc273a
Lucas Anselmo <30813248+lucas62@users.noreply.github.com>
2022-07-27 18:27:32 -0300

    Pass price id to subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss'
 
-export function SubscribeButton() {
+interface SubscribeBttonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeBttonProps) {
     return (
         <button
             type="button"
